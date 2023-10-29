2ef75946dc3d8393042be51f57557826d9fe4b27
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-10-29 17:27:45 +0530

    update the home page and add the rooms section

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@ $(document).ready(function() {
     $('#CheckIn').datepicker();
     $('#CheckOut').datepicker();
 });
+
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 80,
+        responsiveClass: true,
+        center: true,
+        nav: true,
+        autoplay:true,
+        navText: [
+            "<i class='bi bi-arrow-left'></i>",
+            "<i class='bi bi-arrow-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1.4,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 2,
+            }
+        }
+    })
+})
