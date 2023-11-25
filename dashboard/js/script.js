9e51874259fbe81971baa7d82822f7fa62e2c72b
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-11-26 03:31:46 +0530

    add the myBooking page

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -1,55 +1,23 @@
-/*==== result-chart =====*/
-var ctx = document.getElementById('bar-chart');
-Chart.defaults.global.defaultFontFamily = 'Arial';
-Chart.defaults.global.defaultFontSize = 14;
-Chart.defaults.global.defaultFontStyle = '500';
-Chart.defaults.global.defaultFontColor = '#808996';
-
-var chart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    datasets: [{
-      label: "One",
-      data: [40, 35, 54, 40, 45, 60, 70, 65, 70, 90],
-      backgroundColor: '#287dfa',
-      hoverBackgroundColor: '#2273e5',
-      pointBackgroundColor: '#fff',
-      borderWidth: 0,
-      pointRadius: 4
-    }]
-  },
-  options: {
-    tooltips: {
-      backgroundColor: '#1c2540'
-    },
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        barPercentage: 0.4,
-        barThickness: 15,
-        display: true,
-        gridLines: {
-          offsetGridLines: false,
-          display: false
-        }
-      }],
-      yAxes: [{
-        display: true,
-        gridLines: {
-          color: '#eee',
-        },
-        ticks: {
-          fontSize: 14,
-        }
-      }]
-    },
-    elements: {
-      bar: {
-        borderRadius: 20 // Adjust the value based on your preference
+new DataTable('#example', {
+  columnDefs: [
+      {
+          targets: [0],
+          orderData: [0, 1]
+      },
+      {
+          targets: [1],
+          orderData: [1, 0]
+      },
+      {
+          targets: [4],
+          orderData: [4, 0]
       }
-    }
-  }
+  ]
+});
+let table = new DataTable('#example');
+ 
+table.on('click', 'tbody tr', function () {
+    let data = table.row(this).data();
+ 
+    alert('You clicked on ' + data[0] + "'s row");
 });
