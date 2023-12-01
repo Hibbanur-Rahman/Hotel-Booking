385304f8628927c1a204df4b0ced3d1f494e694f
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-12-02 02:04:35 +0530

    add the setting and the wishlist

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -1,23 +1,32 @@
-new DataTable('#example', {
-  columnDefs: [
-      {
-          targets: [0],
-          orderData: [0, 1]
-      },
-      {
-          targets: [1],
-          orderData: [1, 0]
-      },
-      {
-          targets: [4],
-          orderData: [4, 0]
-      }
-  ]
-});
-let table = new DataTable('#example');
+// new DataTable('#example', {
+//   columnDefs: [
+//       {
+//           targets: [0],
+//           orderData: [0, 1]
+//       },
+//       {
+//           targets: [1],
+//           orderData: [1, 0]
+//       },
+//       {
+//           targets: [4],
+//           orderData: [4, 0]
+//       }
+//   ]
+// });
+// let table = new DataTable('#example');
  
-table.on('click', 'tbody tr', function () {
-    let data = table.row(this).data();
+// table.on('click', 'tbody tr', function () {
+//     let data = table.row(this).data();
  
-    alert('You clicked on ' + data[0] + "'s row");
-});
+//     alert('You clicked on ' + data[0] + "'s row");
+// });
+
+// $(":input").inputmask();
+// $("#phone").inputmask({"mask": "(999) 999-9999"});
+
+
+// var profilePhotoUpload=document.querySelector('#profilePhotoUpload');
+// profilePhotoUpload.addEventListener('focus',()=>{
+//     profilePhotoUpload.type='file';
+// })
