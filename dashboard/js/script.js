21999097b609ce37eaeddb0d226c6fd6e7088530
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2023-12-03 02:58:02 +0530

    add the toolpit of profile

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -30,3 +30,18 @@
 // profilePhotoUpload.addEventListener('focus',()=>{
 //     profilePhotoUpload.type='file';
 // })
+
+var profileToolPit=document.querySelector('.profileToolPit');
+var profileImgLeft=document.querySelector('.profileImgLeft');
+
+profileImgLeft.addEventListener('click',()=>{
+    console.log('hello img')
+    console.log(profileToolPit.classList.contains('d-none'))
+    if(profileToolPit.classList.contains('d-none')){
+        profileToolPit.classList.remove('d-none');
+    }
+    else{
+        profileToolPit.classList.add('d-none');
+
+    }
+})
